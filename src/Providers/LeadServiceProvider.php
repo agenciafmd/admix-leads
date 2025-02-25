1a3b4f1d3e47cba43ffebc299580e3dba0441235
Irineu Junior <irineu@fmd.ag>
2025-02-25 14:29:33 -0300

    feat(providers) convenciona o prefixo load e o register

--- src/Providers/LeadServiceProvider.php
+++ src/Providers/LeadServiceProvider.php
@@ -8,21 +8,21 @@ class LeadServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->providers();
+        $this->bootProviders();
 
-        $this->loadMigrations();
+        $this->bootMigrations();
 
-        $this->loadTranslations();
+        $this->bootTranslations();
 
-        $this->publish();
+        $this->bootPublish();
     }
 
     public function register(): void
     {
-        $this->loadConfigs();
+        $this->registerConfigs();
     }
 
-    private function providers(): void
+    private function bootProviders(): void
     {
         $this->app->register(BladeServiceProvider::class);
         $this->app->register(CommandServiceProvider::class);
@@ -31,7 +31,7 @@ class LeadServiceProvider extends ServiceProvider
         $this->app->register(LivewireServiceProvider::class);
     }
 
-    private function publish(): void
+    private function bootPublish(): void
     {
         $this->publishes([
             __DIR__ . '/../../config' => base_path('config'),
@@ -46,18 +46,18 @@ class LeadServiceProvider extends ServiceProvider
         ], ['admix-leads:translations', 'admix-translations']);
     }
 
-    private function loadMigrations(): void
+    private function bootMigrations(): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
     }
 
-    private function loadTranslations(): void
+    private function bootTranslations(): void
     {
         $this->loadTranslationsFrom(__DIR__ . '/../../lang', 'admix-leads');
         $this->loadJsonTranslationsFrom(__DIR__ . '/../../lang');
     }
 
-    private function loadConfigs(): void
+    private function registerConfigs(): void
     {
         $this->mergeConfigFrom(__DIR__ . '/../../config/admix-leads.php', 'admix-leads');
         $this->mergeConfigFrom(__DIR__ . '/../../config/audit-alias.php', 'audit-alias');
